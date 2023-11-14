87c6804af5aa53809795519473f3ad05aee45a3c
André Oliveira <andreaoliveira@ua.pt>
2023-11-14 15:26:19 +0000

    error flights fixed

--- GateMate/webapp/frontend/src/pages/Flight.jsx
+++ GateMate/webapp/frontend/src/pages/Flight.jsx
@@ -1,36 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import useFetch from "../hooks/useFetch";
 import FlightInfo from "../components/FlightInfo";
 
-function Flight() {
-  const { id } = useParams();
-  /* const { data: flight, error, isPending } = useFetch(); */
-  const [flight, setFlight] = useState(null);
+function Flight(props) {
+  const location = useLocation();
+  const flightData = location.state?.flightData;
 
-  const flightsData = [
-    {
-      id: 1,
-      origin: "LIS",
-      destination: "FRA",
-      flightCode: "TP474",
-      airline: "TAP Air Portugal",
-    },
-    {
-      id: 2,
-      origin: "MAD",
-      destination: "US",
-      flightCode: "UX1157",
-      airline: "Air EUROPA",
-    },
-  ];
-
-  useEffect(() => {
-    const selectedFlight = flightsData.find((f) => f.id === parseInt(id));
-    setFlight(selectedFlight);
-  }, [id]);
+  if (!flightData) {
+    // Lida com a falta de dados, como redirecionar para outra página ou exibir uma mensagem de erro
+    return <p>Dados de voo não encontrados</p>;
+  }
 
   return (
     <>
@@ -43,24 +24,24 @@ function Flight() {
           <div className="h-10 bg-white"></div>
           {/* {isPending && <div>Loading...</div>}
           {error && <div>{error}</div>} */}
-          {flight && (
+          {flightData && (
             <div>
               <div className="bg-sky-950 text-white flex flex-col items-center justify-center mx-5">
                 <div className="mt-7">
                   <p className="text-4xl">
-                    {flight.origin} - {flight.destination}
+                    {flightData.origin} - {flightData.destination}
                   </p>
                 </div>
                 <div className="mt-2 mb-16">
                   <p className="text-xs">
-                    {flight.flightCode} - {flight.airline}
+                    {flightData.flightCode} - {flightData.airline}
                   </p>
                 </div>
               </div>
               <div>
-                <FlightInfo flight={flight} />
+                <FlightInfo flight={flightData} />
               </div>
-              <div className="flex justify-center">
+              <div className="flex justify-center mb-10">
                 <button className="btn btn-primary"> Subscribe Flight </button>
               </div>
             </div>
