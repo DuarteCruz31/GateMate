cac982775712d83738509da8c2134541629715f0
BrunoPascoa76 <brunopascoa03@gmail.com>
2023-11-25 11:30:40 +0000

    Should be done

---
+++ GateMate/webapp/src/main/java/projetoIES/webapp/FlightRepository.java
@@ -0,0 +1,9 @@
+package projetoIES.webapp;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface FlightRepository extends MongoRepository<Flight,Integer> {
+    List<Flight> findAll();
+}
---
+++ GateMate/webapp/src/main/java/projetoIES/webapp/LiveData.java
@@ -0,0 +1,16 @@
+package projetoIES.webapp;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class LiveData {
+    private float latitude;
+    private float longitude;
+    private float altitude;
+    private float direction;
+    private float speed_horizontal;
+    private float speed_vertical;
+    private boolean is_ground;
+}
