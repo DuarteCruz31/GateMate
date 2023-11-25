844d85338de213eea67c20a8b73004d896cbe31f
André Oliveira <andreaoliveira@ua.pt>
2023-11-25 23:23:48 +0000

    db adaptor done

--- GateMate/api_consumer/app.py
+++ GateMate/api_consumer/app.py
@@ -1,10 +1,14 @@
-from asyncio import sleep
+import logging
+import asyncio
 import requests
 import json
 import pika
 from pika.exceptions import ChannelWrongStateError
 import datetime
 import os
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 async def data_grabber(api_key, channel, connection):
@@ -14,30 +18,32 @@ async def data_grabber(api_key, channel, connection):
                 f"http://api.aviationstack.com/v1/flights?access_key={api_key}"
             )
 
-            # send data to RabbitMQ
-            channel.basic_publish(
-                exchange="",
-                routing_key="aviation_data",
-                body=json.dumps(response.json()),
-            )
+            data = json.loads(response.text)
+            total = data["pagination"]["total"]
+            num_pages = total // 100 + 1
 
-            print("Data successfully grabbed at {}".format(datetime.datetime.now()))
+            for i in range(num_pages):
+                response = requests.get(
+                    f"http://api.aviationstack.com/v1/flights?access_key={api_key}&offset={i*100}"
+                )
 
-            await sleep(60)
+                # send data to RabbitMQ
+                channel.basic_publish(
+                    exchange="",
+                    routing_key="aviation_data",
+                    body=json.dumps(response.json()),
+                )
+
+                logger.info("Data successfully grabbed at %s", datetime.datetime.now())
+
+            await asyncio.sleep(300)
         except ChannelWrongStateError as e:
-            print(f"Channel error: {e}")
+            logger.info(f"Channel error: {e}")
             if not connection.is_closed:
                 channel = connection.channel()
                 channel.queue_declare(queue="aviation_data")
         except Exception as e:
-            print(f"Error: {e}")
-
-        finally:
-            # Close the connection only when needed
-            if not connection.is_closed:
-                connection.close()
-
-            await sleep(60)
+            logger.error(f"Error: {e}")
 
 
 if __name__ == "__main__":
@@ -56,8 +62,6 @@ if __name__ == "__main__":
     channel.queue_declare(queue="aviation_data")
 
     try:
-        import asyncio
-
         asyncio.run(data_grabber(api_key, channel, connection))
     except KeyboardInterrupt:
         connection.close()
--- GateMate/webapp/src/main/java/projetoIES/webapp/FlightRepository.java
+++ GateMate/webapp/src/main/java/projetoIES/webapp/FlightRepository.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface FlightRepository extends MongoRepository<Flight,Integer> {
+public interface FlightRepository extends MongoRepository<Flight, Integer> {
     List<Flight> findAll();
 }
