9e5ed7dc6a920e3a1985c4a6e91ba14e7390933a
André Oliveira <andreaoliveira@ua.pt>
2023-11-28 03:03:26 +0000

    Update FlightInfo.jsx

--- GateMate/frontend/src/components/FlightInfo.jsx
+++ GateMate/frontend/src/components/FlightInfo.jsx
@@ -3,18 +3,18 @@ import plane from "../assets/plane.png";
 
 function FlightInfo({ flight }) {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 m-4 flex flex-row items-center justify-between">
-      <div>
+    <div className="bg-white rounded-lg shadow-md p-6 m-4 flex flex-row items-center justify-between w-full">
+      <div className="w-1/5 text-center">
         <p className="font-bold text-4xl">{flight.airline_name}</p>
       </div>
-      <div className="card-body flex flex-row text-center">
+      <div className="card-body flex flex-row text-center w-4/5">
         <div className="w-1/4 flex items-center">
           <p className="font text-4xl">{flight.departure.airport}</p>
         </div>
-        <div className="w-2/4 flex flex-row items-center">
+        <div className="w-2/4 flex flex-row items-center ml-6">
           <p className="bg-black h-1 w-[20%]" />
           <img src={plane} alt="Plane" />
-          <p className="bg-black h-1 w-[80%]" />
+          <p className="bg-black h-1 w-[60%]" />
         </div>
         <div className="w-1/4 flex items-center">
           <p className="font text-4xl">{flight.arrival.airport}</p>
