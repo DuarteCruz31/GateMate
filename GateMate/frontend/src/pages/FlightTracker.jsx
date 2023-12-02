7bfbe5cf86b30273aa4ff36c1339b89c5a8a09cc
André Oliveira <andreaoliveira@ua.pt>
2023-12-02 22:33:52 +0000

    Update FlightTracker.jsx

--- GateMate/frontend/src/pages/FlightTracker.jsx
+++ GateMate/frontend/src/pages/FlightTracker.jsx
@@ -11,8 +11,13 @@ import "leaflet-rotatedmarker";
 
 function FlightTracker() {
   const [map, setMap] = useState(null);
-  const { error, isPending, data: flights } = useFetch("http://localhost:8080/api/livedata");
-  const position = [38.7800, -9.1350];
+  const {
+    error,
+    isPending,
+    data: flights,
+  } = useFetch("http://localhost:8080/api/allflights");
+  console.log(flights);
+  const position = [38.78, -9.135];
 
   const planeIcon = new Icon({
     iconUrl: "../plane.webp",
@@ -21,10 +26,10 @@ function FlightTracker() {
   });
 
   const calculateRotation = (direction) => {
-    direction = (direction + 180) % 360 - 180;
-  
+    direction = ((direction + 180) % 360) - 180;
+
     return direction;
-  }
+  };
 
   useEffect(() => {
     // console.log(flights);
@@ -52,39 +57,66 @@ function FlightTracker() {
             <Filter type={"City"} />
           </div>
         </div>
-        <div className="flex-1 ml-4 mr-10" style={{ height: '768px', width: '50%'}}>
-          <MapContainer center={[38.7800, -9.1350]} zoom={13} scrollWheelZoom={false}>
+        <div
+          className="flex-1 ml-4 mr-10"
+          style={{ height: "768px", width: "50%" }}
+        >
+          <MapContainer
+            center={[38.78, -9.135]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-            {flights && flights.map((flight) => (
-              //console.log(flight),
-              <Marker
-              key={flight.flight_number}
-              position={[flight.latitude, flight.longitude]}
-              icon={planeIcon}
-              rotationAngle={calculateRotation(flight.direction + 45)}
-            >
-              <Popup>
-                <Link to={`/flightInfo/${flight.flight_number}`} state={{ flightData: flight }}>
-                  <div className="flex flex-col">
-                    <div className="text-xl font-bold">{flight.flight_number}</div>
-                    <div className="text-sm">{flight.airline}</div>
-                    <div className="text-sm">{flight.plane_type}</div>
-                    <div className="text-sm">{flight.city}</div>
-                    <div className="text-sm">Altitude: {flight.altitude}</div>
-                    <div className="text-sm">Arrival Airport: {flight.arrival_airport}</div>
-                    <div className="text-sm">Departure Airport: {flight.departure_airport}</div>
-                    <div className="text-sm">Direction: {flight.direction}</div>
-                    <div className="text-sm">Speed: {flight.speed}</div>
-                    <div className="text-sm">Vertical Speed: {flight.vertical_speed}</div>
-                  </div>
-                </Link>
-              </Popup>
-            </Marker>
-            
-            ))}
+            {flights &&
+              flights.map((flight) => (
+                //console.log(flight),
+                <Marker
+                  key={flight.flightNumber}
+                  position={[
+                    flight.liveData.latitude,
+                    flight.liveData.longitude,
+                  ]}
+                  icon={planeIcon}
+                  rotationAngle={calculateRotation(
+                    flight.liveData.direction + 45
+                  )}
+                >
+                  <Popup>
+                    <Link
+                      to={`/flightInfo/${flight.flightNumber}`}
+                      state={{ flightData: flight }}
+                    >
+                      <div className="flex flex-col">
+                        <div className="text-xl font-bold">
+                          {flight.flightNumber}
+                        </div>
+                        <div className="text-sm">{flight.airlineName}</div>
+                        <div className="text-sm">
+                          Altitude: {flight.liveData.altitude}
+                        </div>
+                        <div className="text-sm">
+                          Arrival Airport Iata: {flight.arrival.iata}
+                        </div>
+                        <div className="text-sm">
+                          Departure Airport Iata: {flight.departure.iata}
+                        </div>
+                        <div className="text-sm">
+                          Direction: {flight.liveData.direction}
+                        </div>
+                        <div className="text-sm">
+                          Speed: {flight.liveData.speed}
+                        </div>
+                        <div className="text-sm">
+                          Vertical Speed: {flight.liveData.vertical_speed}
+                        </div>
+                      </div>
+                    </Link>
+                  </Popup>
+                </Marker>
+              ))}
           </MapContainer>
         </div>
       </div>
