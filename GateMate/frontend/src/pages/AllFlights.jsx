d804a30fc7a97a513df7733c7b91cb80d0f0985b
André Oliveira <andreaoliveira@ua.pt>
2023-12-02 15:40:12 +0000

    Update AllFlights.jsx

--- GateMate/frontend/src/pages/AllFlights.jsx
+++ GateMate/frontend/src/pages/AllFlights.jsx
@@ -81,7 +81,7 @@ function AllFlights() {
                   key={flight.flight_number}
                   state={{ flightData: flight }}
                 >
-                  <FlightCard flight={flight} />
+                  <FlightCard flight={flight.flight_number} />
                 </Link>
               ))}
           </div>
