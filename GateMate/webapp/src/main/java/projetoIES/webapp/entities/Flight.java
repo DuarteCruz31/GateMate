a4fe4357547973385008bc7e2a28a6bf17d1654c
André Oliveira <andreaoliveira@ua.pt>
2023-12-02 16:43:03 +0000

    Update Flight.java

--- GateMate/webapp/src/main/java/projetoIES/webapp/entities/Flight.java
+++ GateMate/webapp/src/main/java/projetoIES/webapp/entities/Flight.java
@@ -12,7 +12,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "flights")
 public class Flight {
     private int flightNumber;
@@ -26,15 +29,15 @@ public class Flight {
     @Field("live_data")
     private LiveData liveData;
 
-    //object mapping from api response
-    public Flight(JsonNode json, LiveData liveFata){
-        this.flightNumber=json.get("flight_number").asInt();
-        this.flightIata=json.get("flight_iata").asText();
-        this.airlineIata=json.get("airline_iata").asText();
-        this.aircraftRegistration=json.get("reg_number").asText();
-        //no need to change the live data
-        this.liveData=liveData;
-        departure=new AirportFlight();
+    // object mapping from api response
+    public Flight(JsonNode json, LiveData liveData) {
+        this.flightNumber = json.get("flight_number").asInt();
+        this.flightIata = json.get("flight_iata").asText();
+        this.airlineIata = json.get("airline_iata").asText();
+        this.aircraftRegistration = json.get("reg_number").asText();
+        // no need to change the live data
+        this.liveData = liveData;
+        departure = new AirportFlight();
         departure.setIata(json.get("dep_iata").asText());
         departure.setIcao(json.get("dep_icao").asText());
         departure.setTerminal(json.get("dep_terminal").asText());
@@ -43,13 +46,13 @@ public class Flight {
         departure.setActual(json.get("dep_time").asText());
         departure.setEstimated(json.get("dep_estimated").asText(departure.getActual()));
 
-        arrival=new AirportFlight();
+        arrival = new AirportFlight();
         arrival.setIata(json.get("arr_iata").asText());
         arrival.setIcao(json.get("arr_icao").asText());
         arrival.setTerminal(json.get("arr_terminal").asText());
         arrival.setGate(json.get("arr_gate").asText());
         arrival.setDelay(json.get("arr_delayed").asInt(0));
         arrival.setActual(json.get("arr_time").asText());
-        arrival.setEstimated(json.get("arr_estimated").asText(arrival.getActual()));        
+        arrival.setEstimated(json.get("arr_estimated").asText(arrival.getActual()));
     }
 }
