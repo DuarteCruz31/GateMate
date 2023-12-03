4e9863008ad28d04789776f381a6666e3a47ffb9
André Oliveira <andreaoliveira@ua.pt>
2023-12-03 01:12:11 +0000

    done

--- GateMate/frontend/src/pages/FlightTracker.jsx
+++ GateMate/frontend/src/pages/FlightTracker.jsx
@@ -5,20 +5,68 @@ import "../../node_modules/leaflet/dist/leaflet.css";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Filter from "../components/Filter";
 import useFetch from "../hooks/useFetch";
 import airplaneIcon from "../assets/plane.webp";
 import "leaflet-rotatedmarker";
 
 function FlightTracker() {
-  const [map, setMap] = useState(null);
   const {
-    error,
+    data: flightsData,
     isPending,
-    data: flights,
+    error,
   } = useFetch("http://localhost:8080/api/allflights");
 
-  const position = [38.78, -9.135];
+  const [flights, setFlights] = useState([]);
+  const [flightNotFound, setFlightNotFound] = useState(false);
+
+  useEffect(() => {
+    setFlights(flightsData || []);
+  }, [flightsData]);
+
+  const [filter, setFilter] = useState({
+    flightNumber: "",
+    from: "",
+    to: "",
+    company: "",
+  });
+
+  function resetFilters() {
+    setFilter({
+      flightNumber: "",
+      from: "",
+      to: "",
+      company: "",
+    });
+    setFlights(flightsData);
+  }
+
+  async function handleSearch() {
+    try {
+      var url = "http://localhost:8080/api/allflights?";
+      if (filter.from != "") {
+        url += `from=${filter.from}&`;
+      }
+      if (filter.to != "") {
+        url += `to=${filter.to}&`;
+      }
+      if (filter.company != "") {
+        url += `company=${filter.company}&`;
+      }
+      if (filter.flightNumber != "") {
+        url += `flightNumber=${filter.flightNumber}&`;
+      }
+
+      console.log(url);
+      const flightsDataFiltered = await fetch(url).then((res) => res.json());
+
+      setFlights(flightsDataFiltered ? flightsDataFiltered : []);
+      setFlightNotFound(false);
+    } catch (error) {
+      setFlightNotFound(true);
+      setFlights([]);
+      console.error("Erro ao buscar voos:", error);
+    }
+  }
 
   const planeIcon = new Icon({
     iconUrl: airplaneIcon,
@@ -40,18 +88,120 @@ function FlightTracker() {
       <div className="flex flex-row">
         <div className="w-1/3 p-5 mt-7 flex flex-col">
           <div>
-            <button className="text-blue-400">
+            <button className="text-blue-400" onClick={() => resetFilters()}>
               <u>Reset all</u>
             </button>
           </div>
           <div className="pb-5 pt-2">
-            <Filter type={"Company"} />
+            <div className="font-bold">Flight Number</div>
+            <hr />
+            <div className="mt-3">
+              <input
+                className="pl-2 appearance-none bg-gray-100 text-zinc-600 w-full h-10 text-xl font-normal outline-none inline-flex"
+                type="text"
+                name="flightCode"
+                id="flightCode"
+                placeholder=" Code"
+                value={filter.flightNumber}
+                onChange={(e) =>
+                  setFilter({ ...filter, flightNumber: e.target.value })
+                }
+              />
+            </div>
           </div>
           <div className="py-5">
-            <Filter type={"PlaneType"} />
+            <div>
+              <div className="font-bold">Company</div>
+              <hr />
+              <div className="mt-3">
+                <select
+                  className="pl-2 appearance-none bg-gray-100 text-zinc-600 w-full h-10 text-xl font-normal outline-none inline-fle"
+                  value={filter.company}
+                  onChange={(e) =>
+                    setFilter({ ...filter, company: e.target.value })
+                  }
+                >
+                  <option value="" selected>
+                    Company
+                  </option>
+                  {flightsData &&
+                    [
+                      ...new Set(
+                        flightsData.map((flight) => flight.airlineName)
+                      ),
+                    ].map((uniqueName) => (
+                      <option key={uniqueName} value={uniqueName}>
+                        {uniqueName}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </div>
           </div>
           <div className="py-5">
-            <Filter type={"City"} />
+            <div>
+              <div className="font-bold">From</div>
+              <hr />
+              <div className="mt-3">
+                <select
+                  className="pl-2 appearance-none bg-gray-100 text-zinc-600 w-full h-10 text-xl font-normal outline-none inline-flex"
+                  value={filter.from}
+                  onChange={(e) =>
+                    setFilter({ ...filter, from: e.target.value })
+                  }
+                >
+                  <option value="" selected>
+                    From
+                  </option>
+                  {flightsData &&
+                    [
+                      ...new Set(
+                        flightsData.map((flight) => flight.departure.iata)
+                      ),
+                    ].map((uniqueIata) => (
+                      <option key={uniqueIata} value={uniqueIata}>
+                        {uniqueIata}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="py-5">
+            <div>
+              <div className="font-bold">To</div>
+              <hr />
+              <div className="mt-3">
+                <select
+                  className="pl-2 appearance-none bg-gray-100 text-zinc-600 w-full h-10 text-xl font-normal outline-none inline-fle"
+                  value={filter.to}
+                  onChange={(e) => setFilter({ ...filter, to: e.target.value })}
+                >
+                  <option value="" selected>
+                    To
+                  </option>
+                  {flightsData &&
+                    [
+                      ...new Set(
+                        flightsData.map((flight) => flight.arrival.iata)
+                      ),
+                    ].map((uniqueIata) => (
+                      <option key={uniqueIata} value={uniqueIata}>
+                        {uniqueIata}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-5">
+            <button
+              className="w-full bg-blue-700 rounded justify-center items-center inline-flex px-12 py-2 text-center text-white text-base font-bold leading-normal"
+              onClick={() => handleSearch()}
+            >
+              Apply filters
+            </button>
           </div>
         </div>
         <div
