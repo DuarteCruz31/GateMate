76033acebf9f6dbba569effa535396ad82a7799a
André Oliveira <andreaoliveira@ua.pt>
2023-12-11 18:28:19 +0000

    Update app.py

--- GateMate/notification_manager/app.py
+++ GateMate/notification_manager/app.py
@@ -9,69 +9,14 @@ from pymongo import MongoClient
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-""" import webapp.src.main.java.projetoIES.webapp.services.AuthenticationService as auth
+
+def sent_email(data, collection):
+    data = data.decode()
+    print(data)
+    print("Email sent")
 
 
-# def connect_to_redis():
-#     return redis.StrictRedis(host='redis', port=6379, decode_responses=True)
-
-def get_email(token):
-    user = auth.validateToken(token)
-    return user.email
-
-
-def get_flight_info(iata):
-    base_url = os.getenv("http://localhost:8083")  # Replace with the base URL of your API
-    api_url = f"{base_url}/api/flight/{iata}"
-    
-    try:
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            return response.json()  # Assuming the API returns JSON data for the flight
-        elif response.status_code == 404:
-            print(f"Flight with IATA code {iata} not found")
-            return None
-        else:
-            print(f"Failed to fetch data for flight {iata}. Status code: {response.status_code}")
-            return None
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
-        return None
-    
-def get_user_info(token):
-
-
-def check(dictionary):
-    while True:
-        for key in dictionary:
-            if key == flight:
-                for value in dictionary[key]:
-                    if value == key:
-                        notifications(user, flight)
-
-def notifications(user, flight):
-    # while True:
-        try:
-            flight_info = get_flight_info(flight["iata"])
-            # Check if the flight info was found
-            if flight_info is not None:
-                # Check if the flight is delayed
-                if flight_info["status"] == "DELAYED":
-                    # Send a notification to the user
-                    print(f"Flight {flight_info['iata']} is delayed!")
-                else:
-                    print(f"Flight {flight_info['iata']} is on time")
-            else:
-                print(f"Flight {flight['iata']} not found")
-            # Wait for 5 seconds
-            time.sleep(5)
-        except requests.RequestException as e:
-            print(f"Request failed: {e}")
-            # Wait for 5 seconds
-            time.sleep(5) """
-
-
-def start_server():
+def start_server(collection):
     PORT = 1234
     ADDRESS = "0.0.0.0"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -84,7 +29,7 @@ def start_server():
         conn, addr = s.accept()
         logging.info(f"Connection from {addr} accepted!")
         data = conn.recv(1024)
-        logging.info(f"Received {data} from {addr}")
+        sent_email(data, collection)
         conn.close()
 
 
@@ -95,4 +40,4 @@ if __name__ == "__main__":
         db.create_collection("subscribed_flights")
     collection = db["subscribed_flights"]
 
-    threading.Thread(target=start_server).start()
+    threading.Thread(target=start_server(collection)).start()
